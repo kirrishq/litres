13546240c71a0e1f2a72a091ee9c9e0dbd891acd
kirrishq <79932859+kirrishq@users.noreply.github.com>
2026-05-12 04:59:54 +0300

    fixed errors

--- app/components/cards/FundCard.tsx
+++ app/components/cards/FundCard.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image'
 import { useRef } from 'react'
 import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
 import { useGSAP } from '@gsap/react'
 import { Button } from '../ui/Button'
 
